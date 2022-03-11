57d7060bfe72ce4b58571399d01c17e5a4fb89ff
SBSun <46879319+SBSun@users.noreply.github.com>
2022-03-11 23:28:27 +0900

    Update 2839.cpp

--- Greedy/2839.cpp
+++ Greedy/2839.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+#pragma warning(disable:4996)
+
+int main() {
+	int N;
+	int count = 0;
+
+	scanf("%d", &N);
+
+	while (N >= 0)
+	{
+		if (N % 5 == 0)
+		{
+			count += N / 5;
+			printf("%d\n", count);
+			return 0;
+		}
+
+		N -= 3;
+		count++;
+	}
+
+	printf("-1\n");
+}
